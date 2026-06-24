a441eaa9545a52b4f4de39c09a230603532d4fce
DHEERENDRA <lifesstudy@gmail.com>
2026-06-24 20:12:01 +0530

    Fix AGP namespace issue for older plugins like play_integrity_flutter

--- flutter/student_app/android/build.gradle.kts
+++ flutter/student_app/android/build.gradle.kts
@@ -24,6 +24,20 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    afterEvaluate {
+        val androidExt = extensions.findByName("android")
+        if (androidExt != null) {
+            val ns = try {
+                androidExt.javaClass.getMethod("getNamespace").invoke(androidExt)
+            } catch (e: Exception) { null }
+            if (ns == null) {
+                try {
+                    androidExt.javaClass.getMethod("setNamespace", String::class.java).invoke(androidExt, project.group.toString())
+                } catch (e: Exception) { }
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
