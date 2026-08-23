a27dd0620125aca1364d5e94f9729f3fa5d886d7
Dheerendra Tripathi <153520649+acharypdt@users.noreply.github.com>
2026-08-23 20:51:14 +0530

    fix: make VideoPlayerOptions non-const

--- flutter/student_app/lib/screens/course_detail_screen.dart
+++ flutter/student_app/lib/screens/course_detail_screen.dart
@@ -632,7 +632,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen>
       final controller = VideoPlayerController.networkUrl(
         Uri.parse(widget.videoUrl),
         httpHeaders: headers,
-        videoPlayerOptions: const VideoPlayerOptions(),
+        videoPlayerOptions: VideoPlayerOptions(),
       );
       _videoPlayerController = controller;
 
